2f0409f86202bcecee1d95fe67252a52a010d262
Bruno_Ss <brunosch250@gmail.com>
2025-11-26 14:47:16 -0300

    Update all Services

--- src/api/atividadeService.js
+++ src/api/atividadeService.js
@@ -1,4 +1,3 @@
-// src/api/atividadeService.js
 import axiosClient from "./axiosClient";
 
 /**
--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,4 +1,3 @@
-// src/api/axiosClient.js
 import axios from "axios";
 
 const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:8080";
